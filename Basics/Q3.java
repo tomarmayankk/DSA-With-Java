76e2d444a9f821a0089b37acd2f1a3f0320783a3
tomarmayankk <mayanktomarpc@gmail.com>
2024-05-01 18:23:13 +0530

    Question updated

---
+++ Basics/Q3.java
@@ -0,0 +1,15 @@
+package Basics;
+//there is an array of numbers containing the negative duplicates but there is a number with no duplicates find the number
+public class Q3 {
+    public static int FindUnique(int arr[]){
+        int unique = 0;
+        for (int n: arr){
+            unique += n;
+        }
+        return unique;
+    }
+    public static void main(String[] args){
+        int [] arr = {-2, -3, 3, -4, 2, 6, 4};
+        System.out.println(FindUnique(arr));
+    }
+}
