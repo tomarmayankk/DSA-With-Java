5880c1ce4cc0e18744786e91f2178e237200fed1
tomarmayankk <mayanktomarpc@gmail.com>
2024-06-27 12:32:14 +0530

    added more operation in linked list

--- Linkedlist/Basic.java
+++ Linkedlist/Basic.java
@@ -8,7 +8,7 @@ class Node{
     }
 }
 //initalize a linked list class
-public class Basic {
+public class Basic { //this basic represents a linked list class
     private Node head;
 
     public Basic(){
@@ -43,13 +43,64 @@ public class Basic {
             current = current.next;
         }
     }
-
-
+    //method to delete a node with specific data
+    public boolean delete(int data){
+        if(head == null){
+            return false;
+        }
+        if(head.data == data){
+            head = head.next;
+            return true;
+        }
+        Node current = head;
+        while(current.next != null){
+            if(current.next.data == data){
+                current.next = current.next.next;
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+    //method to reverse a linked list
+    public void reverse(){
+        Node previous = null;
+        Node current = head;
+        Node next = null;
+        while(current != null){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+    }
+    public int search(int data){
+        int count = 0;
+        Node current = head;
+        while (current!= null) {
+            if(current.data == data){
+                return count;
+            }  
+            current = current.next;
+            count++;
+        }
+        return -1;
+    }
     public static void main(String[] args){
             Basic list = new Basic();
             list.insert(22);
             list.insert(24);
             list.insert(23);
+            list.insertBegin(33);
+            System.out.println("list after adding the elements: ");
+            list.display();
+            list.delete(24);
+            System.out.println("list after the deletion of the element: ");
+            list.display();
+            System.out.println("index of element" + list.search(22));
+            list.reverse();
+            System.out.println("list after reversal: ");
             list.display();
     }
 }
